e7a7f93269164935387e157d861ba3f15e43f4f0
TristanBarrow <tristanmarkbarrow@gmail.com>
2024-10-31 08:10:48 -0600

    init

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,12 @@
+
+-- require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  -- ensure_installed = {
+  -- },
+  -- handlers = {
+--    function(server_name)
+--      require('lspconfig')[server_name].setup({})
+--     end,
+--  }
+-- })
